682e4306327ea6d3c8625aa00e58dbaed78eae1a
pwegener <pwegener@circular.de>
2024-12-02 18:03:39 +0100

    quiz 2 doesnt

---
+++ Day02/day2-puz2.py
@@ -0,0 +1,47 @@
+countSafe = 0
+listOfUnsafes = []
+def checkIfIntListIsSafe(intList):
+    inc = False
+    dec = False
+    tmpList = []
+    for i in range(len(intList) - 1):
+        if i == 0:
+            if intList[0]<intList[-1]:
+                inc = True
+            else:
+                dec = True
+
+        diff = intList[i] - intList[i+1]
+
+        if diff == 0:
+            if not len(tmpList) >= 1:
+                tmpList.append(intList[i])
+            else:
+                return False
+        if inc:
+            if diff > 0 or diff < -3:
+                if not len(tmpList) >= 1:
+                    tmpList.append(intList[i])
+                else:
+                    return False
+        if dec:
+            if diff < 0 or diff > 3:
+                if not len(tmpList) >= 1:
+                    tmpList.append(intList[i])
+                else:
+                    return False
+        print(tmpList)
+    
+    return True
+
+with open("input/input.txt", "r") as i:
+    for line in i:
+        # print("############### next line #############")
+        # print(line)
+        intList = list(map(int, line.split()))
+        if checkIfIntListIsSafe(intList):
+            countSafe += 1
+        else:
+            listOfUnsafes.append(intList)
+
+print(countSafe)
