1a7e3b723f955998176aa7e7443b62e225a1a3bf
pwegener <pwegener@circular.de>
2024-12-05 11:43:22 +0100

    day 5 part 1

---
+++ Day05/day5-puz1.py
@@ -0,0 +1,45 @@
+part1Toggle = True
+por = []
+updates = []
+with open("input/input.txt", "r") as i:
+    for line in i:
+        if line == "\n":
+            part1Toggle = False
+            continue
+        if part1Toggle:
+            tmp = tuple(map(int, line.split("|")))
+            por.append(tmp)
+            continue
+        updates.append(list(map(int, line.split(","))))
+        
+# print(por)
+# print(updates)
+
+def checkOrder(page, index, update):
+    rules = [r for r in por if r[0] == page]
+    if index == 0:
+        return True
+    for rule in rules:
+        for i in range(0, index):
+            if update[i] == rule[1]:
+                print(f"# DEBUG: rule wurde nicht erfüllt: {rule}, in line{update}")
+                return False
+    return True
+
+resultList = []
+# toggle = True
+for updateNumber, update in enumerate(updates):
+    toggle = True
+    for index, page in enumerate(update):
+        # print(checkOrder(page, index, por, update))
+        if not checkOrder(page, index, update):
+            toggle = False
+    if toggle:
+        print(f"# DEBUG: Found correct entry in line {updateNumber}")
+        resultList.append(update[int(len(update)/2-0.5)])
+
+
+
+
+# print(resultList)
+print(sum(resultList))
