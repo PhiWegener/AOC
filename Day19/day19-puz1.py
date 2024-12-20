a8caf8b4a74b083f4d11bebad93c8a243692097b
pwegener <pwegener@circular.de>
2024-12-20 12:19:38 +0100

    not working

--- Day19/day19-puz1.py
+++ Day19/day19-puz1.py
@@ -1,4 +1,4 @@
-import time
+import time, re
 st = time.process_time()
 
 patterns = []
@@ -14,22 +14,40 @@ with open("input/input.txt", 'r') as file:
             continue
         designs.append(line.strip())
 
-
+patterns = sorted(patterns, key=len, reverse=True)
 for i in range(len(patterns)):
     # print(entry)
     patterns[i] = patterns[i].strip().replace("\n", "")
+    # if "g" in patterns[i] or len(patterns[i]) == 1:
+    #     patterns.append(patterns[i])
 
-patterns = sorted(patterns, key=len, reverse=True)
+search = "("
+for i, pattern in enumerate(patterns):
+    if i == len(patterns)-1:
+        search += pattern + ")+"
+        continue
+    search += pattern + "|"
+
+cnt = []
 counter = 0
-for design in designs:
-    for pattern in patterns:    
-        if pattern in design:
-            design = design.replace(pattern, "")
-    if design == "":
+for i, design in enumerate(designs):
+    debugOut = design + ": "
+    # for pattern in patterns:    
+    #     if pattern in design:
+    #         design = design.replace(pattern, " ")
+    #     else: 
+    #         debugOut += pattern + "; "
+    #         # print(pattern)
+    design = re.sub(search, " ",design)    
+    if design.strip() == "":
         counter += 1
+    # else:
+    #     print(design + "\n")
+    
 
-# 328 falsches Ergebnis        
+# 328 falsches Ergebnis  
+# 287 falsches Ergebnis      
 print(counter)
-
+print(cnt)
 et = time.process_time()
 print(f"\nZeit: {(et-st)*1000}ms")
