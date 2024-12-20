606f905238972eb2663ee77136c7f66fa0a9da4a
pwegener <pwegener@circular.de>
2024-12-20 13:58:41 +0100

    runs infinitely

--- Day19/day19-puz1.py
+++ Day19/day19-puz1.py
@@ -1,4 +1,5 @@
-import time, re
+import time
+import re
 st = time.process_time()
 
 patterns = []
@@ -8,7 +9,6 @@ with open("input/input.txt", 'r') as file:
     for line in file:
         if "," in line:
             patterns = line.split(",")
-            # print(patterns)
             continue
         if line == "\n":
             continue
@@ -16,38 +16,25 @@ with open("input/input.txt", 'r') as file:
 
 patterns = sorted(patterns, key=len, reverse=True)
 for i in range(len(patterns)):
-    # print(entry)
     patterns[i] = patterns[i].strip().replace("\n", "")
-    # if "g" in patterns[i] or len(patterns[i]) == 1:
-    #     patterns.append(patterns[i])
+    if "g" in patterns[i] or len(patterns[i]) == 1:
+        patterns.append(patterns[i])
 
-search = "("
+search = r"("
 for i, pattern in enumerate(patterns):
     if i == len(patterns)-1:
         search += pattern + ")+"
         continue
     search += pattern + "|"
 
-cnt = []
+search = re.compile(search)
 counter = 0
-for i, design in enumerate(designs):
-    debugOut = design + ": "
-    # for pattern in patterns:    
-    #     if pattern in design:
-    #         design = design.replace(pattern, " ")
-    #     else: 
-    #         debugOut += pattern + "; "
-    #         # print(pattern)
-    design = re.sub(search, " ",design)    
-    if design.strip() == "":
-        counter += 1
-    # else:
-    #     print(design + "\n")
-    
+for design in designs:
+    if search.fullmatch(design):
+        counter += 1   
 
 # 328 falsches Ergebnis  
 # 287 falsches Ergebnis      
 print(counter)
-print(cnt)
 et = time.process_time()
 print(f"\nZeit: {(et-st)*1000}ms")
