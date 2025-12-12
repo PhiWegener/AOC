c10440029ff172cdf48324a38f119a3adb4bdca3
pwegener <pwegener@circular.de>
2025-12-12 15:02:51 +0100

    solution part1

--- 2025/Day04/part1.py
+++ 2025/Day04/part1.py
@@ -2,7 +2,7 @@ import time
 st = time.process_time()
 
 devMode = False
-devMode = True
+# devMode = True
 
 if devMode:
     inputFile = "example.txt"
@@ -10,11 +10,47 @@ else:
     inputFile = "input.txt"
 
 inputList = []
-    
+counter = 0
+
 with open(inputFile, "r") as i:
     for line in i:
-        inputList.append(line)
+        inputList.append(line.strip())
 
+for y, line in enumerate(inputList):
+
+    for x, letter in enumerate(line):
+        amntRolls = 0
+        if  letter == ".":
+            continue
+        # links oben
+        if y-1 >= 0 and x-1 >= 0 and inputList[y-1][x-1] == "@":
+            amntRolls += 1
+        # oben
+        if y-1 >= 0 and inputList[y-1][x] == "@":
+            amntRolls += 1
+        # rechts oben
+        if y-1 >= 0 and x+1 < len(line) and inputList[y-1][x+1] == "@":
+            amntRolls += 1
+        # rechts
+        if x+1 < len(line) and inputList[y][x+1] == "@":
+            amntRolls += 1
+        # rechts unten
+        if y+1 < len(inputList) and x+1 < len(line) and inputList[y+1][x+1] == "@":
+            amntRolls += 1
+        # unten
+        if y+1 < len(inputList) and inputList[y+1][x] == "@":
+            amntRolls += 1
+        # links unten
+        if y+1 < len(inputList) and x-1 >= 0 and inputList[y+1][x-1] == "@":
+            amntRolls += 1
+        # links
+        if x-1 >= 0 and inputList[y][x-1] == "@":
+            amntRolls += 1
+
+        if amntRolls < 4:
+            counter += 1
+
+print(counter)
 
 et = time.process_time()
 print(f"\nZeit: {(et-st)*1000}ms")
