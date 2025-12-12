c58a59d4b6b1098c7ea53a5f55975e85a6e8851f
pwegener <pwegener@circular.de>
2025-12-12 13:07:41 +0100

    init day04

---
+++ 2025/Day04/part1.py
@@ -0,0 +1,20 @@
+import time
+st = time.process_time()
+
+devMode = False
+devMode = True
+
+if devMode:
+    inputFile = "example.txt"
+else:
+    inputFile = "input.txt"
+
+inputList = []
+    
+with open(inputFile, "r") as i:
+    for line in i:
+        inputList.append(line)
+
+
+et = time.process_time()
+print(f"\nZeit: {(et-st)*1000}ms")
